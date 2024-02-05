9670b61a1df89f0e3a60adce9b40851d00afaec2
Dev Dave <devdave5033@gmail.com>
2024-02-05 21:23:34 +0530

    VERSION 11

--- change.js
+++ change.js
@@ -1,1 +1,3 @@
 console.log('this will be added in version 1');
+console.log('version 11');
+console.log('version 12');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('index.js');
-console.log('version1')
+console.log('version1');
+console.log("version 11");
